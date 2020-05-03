1cf21a964e782b087d66abee02c90def400360dc
sharmamonu1515 <sharmamonu1515@gmail.com>
2020-05-03 18:17:25 +0530

    Updates

---
+++ precache-manifest.c1d161b56ed2e758ddaa446f56dc9cc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab232af8a04016f45d326fba997f5103",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "9497ca8d8423e263e431",
+    "url": "/covid19/static/css/main.ef2931b3.chunk.css"
+  },
+  {
+    "revision": "0e5aa606a4cdb4452013",
+    "url": "/covid19/static/js/2.5d25957e.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19/static/js/2.5d25957e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9497ca8d8423e263e431",
+    "url": "/covid19/static/js/main.59ea07ea.chunk.js"
+  },
+  {
+    "revision": "bb08661687ef9ed76af2",
+    "url": "/covid19/static/js/runtime-main.06ba182b.js"
+  }
+]);
